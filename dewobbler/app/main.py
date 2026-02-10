dc55ea23f7fcc529e5273098300296a4e13cb802
Arivald8 <arivald88@gmail.com>
2026-02-10 14:10:13 +0000

    Updated main with new route. Also added to processes

--- dewobbler/app/main.py
+++ dewobbler/app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+
 from app.api.v1 import processes
+from app.api.v1 import debugger
 from app.core.config import settings
 
 templates = Jinja2Templates(directory="app/templates")
@@ -15,6 +17,7 @@ def create_application() -> FastAPI:
     )
 
     app.include_router(processes.router, prefix="/api/v1")
+    app.include_router(debugger.router, prefux="/api/v1")
 
     @app.get("/", response_class=HTMLResponse)
     def index(request: Request):
